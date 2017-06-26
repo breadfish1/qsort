1e99dcb33ef8d2a67194c3bb7be4f2429f4a7680
Aleksey Belyanin <alekseybelyanin@Alekseys-MacBook-Air.local>
2017-06-26 11:34:52 +0300

    Initial Commit

---
+++ Q sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Q sort
+//
+//  Created by Aleksey Belyanin on 26/06/2017.
+//  Copyright © 2017 Aleksey Belyanin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
